cc81da8bd43cc7c0baef3937fff96acb339a62e4
Mateusz Wit <mateuszwit21@gmail.com>
2019-11-03 20:15:53 +0100

    Set-up Figwheel project

---
+++ src/examples_cljs/read-stream.cljs
@@ -0,0 +1,26 @@
+(ns examples-cljs.read-stream)
+
+(defonce ble
+  {:start (fn [controller]
+            (do
+              (js/console.log "starting")
+              (js/setInterval #(.enqueue controller "abc") 1000)))})
+
+(-> (clj->js ble)
+    (js/ReadableStream.)
+    (.getReader)
+    (.read)
+    (#(do (js/console.log "xyz" %1) %1))
+    (.then #(js/console.log %1))
+    (.finally #(js/console.log %1))
+    (js/console.log))
+
+(defonce decoder (js/TextDecoder. "utf-8"))
+
+(-> (js/fetch "/")
+    (.then #(.-body %1))
+    (.then #(.getReader %1))
+    (.then #(do (js/console.log %1) %1))
+    (.then #(.read %1))
+    (.then #(.decode decoder (.-value %1)))
+    (.then js/console.log))
